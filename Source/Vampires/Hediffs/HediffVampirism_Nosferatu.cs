7cfdef596b3479fd85f6c638384828a55aa9415a
jecrell <matt.walls31@gmail.com>
2018-05-20 13:47:20 +0900

    Added HediffGivers for Vampires

---
+++ Source/Vampires/Hediffs/HediffVampirism_Nosferatu.cs
@@ -0,0 +1,7 @@
+﻿namespace Vampire
+{
+    public class HediffVampirism_Nosferatu : HediffVampirism_VampGiver
+    {
+        public override BloodlineDef Bloodline => VampDefOf.ROMV_ClanNosferatu;
+    }
+}
---
+++ Source/Vampires/Hediffs/HediffVampirism_Ventrue.cs
@@ -0,0 +1,7 @@
+﻿namespace Vampire
+{
+    public class HediffVampirism_Ventrue : HediffVampirism_VampGiver
+    {
+        public override BloodlineDef Bloodline => VampDefOf.ROMV_ClanVentrue;
+    }
+}
